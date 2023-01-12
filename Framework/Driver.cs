17b6daa889eff6f5e18b3935ddfb59ae9c83a8cc
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-12 14:22:36 +0200

    add class "Driver" (+methods)

---
+++ Framework/Driver.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public class Driver
+    {
+       private static IWebDriver driver;
+
+        public static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        public static void CreateDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public static void CloseDriver()
+        {
+            driver.Quit();
+        }
+
+        public static void OpenPage(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+    }
+}
